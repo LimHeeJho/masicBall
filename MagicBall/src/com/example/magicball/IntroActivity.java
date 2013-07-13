f7d6ea3ffd069384e109fb7c41d232d85ef12db7
임희조 <hijo@naver.com>
2013-07-13 17:29:25 +0900

    ddddddddd

---
+++ MagicBall/src/com/example/magicball/IntroActivity.java
@@ -0,0 +1,40 @@
+package com.example.magicball;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class IntroActivity extends Activity {
+
+	Handler handler = new Handler();
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.splash);
+
+		handler.postDelayed(new splashHandler(), 3000);
+		// TODO Auto-generated method stub
+	}
+
+	class splashHandler implements Runnable {
+
+		public void run() {
+			Intent i = new Intent(IntroActivity.this, MainActivity.class);
+			startActivity(i);
+			finish();
+			
+			overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+			
+		}
+	}
+	
+	public void onBackPressed() {
+		super.onBackPressed();
+		handler.removeCallbacks(new splashHandler());
+	}
+
+}
